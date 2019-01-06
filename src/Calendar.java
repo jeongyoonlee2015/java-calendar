7f9818c86fd1dbfaf518c19c9af75ea7f5b61e0d
happyjoy2005@gmail.com <happyjoy2005@gmail.com>
2019-01-06 23:29:28 +0900

    git init

---
+++ src/Calendar.java
@@ -0,0 +1,7 @@
+public class Calendar {
+        public static void main(String[] args) {
+            System.out.println("Hello, Calendar");
+            System.out.println("Init");
+
+        }
+}
