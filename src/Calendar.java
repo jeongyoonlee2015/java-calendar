91fb27e20cfb74b0e0b0e7c73d2c649883bf0af2
happyjoy2005@gmail.com <happyjoy2005@gmail.com>
2019-01-07 23:13:34 +0900

    Update using Class Method

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
 public class Calendar {
-        public static void main(String[] args) {
-            System.out.println("");
-            System.out.println("일 월 화 수 목 금 토");
-            System.out.println("--------------------");
-            System.out.println(" 1  2  3  4  5  6  7");
-            System.out.println(" 8  9 10 11 12 13 14");
-            System.out.println("15 16 17 18 19 20 21");
-            System.out.println("22 23 24 25 26 27 28");
+    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+    public int getMaxDaysOfMonth(int month) {
+        return MAX_DAYS[month - 1];
+    }
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("달을 입력해주세요.");
-            int num = scanner.nextInt();
-            int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public void printSampleCalendar(){
+        System.out.println("일 월 화 수 목 금 토");
+        System.out.println("--------------------");
+        System.out.println(" 1  2  3  4  5  6  7");
+        System.out.println(" 8  9 10 11 12 13 14");
+        System.out.println("15 16 17 18 19 20 21");
+        System.out.println("22 23 24 25 26 27 28");
+}
+    public static void main(String[] args) {
 
-            System.out.printf("%d월은 %d일까지 있습니다.\n", num, maxDays[num - 1]);
-            scanner.close();
-        }
+        Scanner scanner = new Scanner(System.in);
+        Calendar cal = new Calendar();
+        System.out.println("달을 입력해주세요.");
+        int month = scanner.nextInt();
+
+        System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+        cal.printSampleCalendar();
+        scanner.close();
+    }
 }
