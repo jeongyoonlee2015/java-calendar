5e5437614c67f538cf41fbdd7b177f274d44e031
happyjoy2005@gmail.com <happyjoy2005@gmail.com>
2019-01-09 22:09:44 +0900

    Virtual Calendar(2)

--- src/Prompt.java
+++ src/Prompt.java
@@ -7,18 +7,24 @@ public class Prompt {
     public void runPrompt(){
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
-        int month = 1;
+        int month = -1;
+        int year = -1;
+
         while (true){
+            System.out.println("년도를 입력해주세요.");
+            System.out.print("YEAR>");
+            year = scanner.nextInt();
             System.out.println("달을 입력해주세요.");
-            System.out.print(PROMPT);
+            System.out.print("MONTH>");
             month = scanner.nextInt();
+
             if (month == -1){
                 break;
             }else if(month < 1 || month > 12){
                 System.out.println("1 ~ 12월을 선택해주세요.");
                 continue;
             }
-            cal.printCalendar(2019, month);
+            cal.printCalendar(year, month);
         }
 
         //cal.printSampleCalendar();
